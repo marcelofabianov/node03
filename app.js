ff8eea048f52f2b7de7fa91ad8beb22f81d534fb
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2020-01-28 16:12:45 -0300

    primeiras models

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://user:secret@localhost:3306/express');
 
-sequelize
-  .authenticate()
-  .then(() => console.log('Success'))
-  .catch(err => console.error(err));
+// sequelize
+//   .authenticate()
+//   .then(() => console.log('Success'))
+//   .catch(err => console.error(err));
+
+const Post = sequelize.define('posts', {
+  'title': {
+    type: Sequelize.STRING
+  },
+  'body': {
+    type: Sequelize.TEXT
+  }
+});
+
+const User = sequelize.define('users', {
+  'name': {
+    type: Sequelize.STRING
+  },
+  'email': {
+    type: Sequelize.STRING
+  },
+  'age': {
+    type: Sequelize.INTEGER
+  }
+});
+
+Post.sync();
+User.sync();
